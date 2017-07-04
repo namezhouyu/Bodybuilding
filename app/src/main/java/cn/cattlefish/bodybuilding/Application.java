f3baea37c5a64bafe5a4b3710a6e7ab14c312928
周晓东 <zhouxiaodong@yaomaitong.cn>
2017-07-04 13:23:52 +0800

    init

---
+++ app/src/main/java/cn/cattlefish/bodybuilding/Application.java
@@ -0,0 +1,66 @@
+package cn.cattlefish.bodybuilding;
+
+
+/*
+* Copyright (C) 2016-2017 Yaomaitong Inc.All Rights Reserved.
+* FileName：Application
+* @Description：简要描述本文件的内容
+* History：
+* v1.0 danggui 2017/7/3 Create
+*/
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Environment;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+public class Application extends android.app.Application {
+  private static final String GROUP_PART_TIME_KEY = "group_part_time_key";//一组的时间
+  private static final String GROUP_REST_TIME_KEY = "group_rest_time_key";//组间休息时间
+  private static final String GROUP_COUNT_KEY = "group_count_key";//组数
+  private SharedPreferences sharedPreferences;
+  private static Application instance;
+  public static final String APP_ID = "52641772a1";
+
+  @Override public void onCreate() {
+    super.onCreate();
+    instance = (Application) getApplicationContext();
+    sharedPreferences = getSharedPreferences("body_building", Context.MODE_PRIVATE);
+    Beta.autoInit = true;
+    Beta.largeIconId = R.mipmap.ic_launcher;
+    Beta.smallIconId = R.mipmap.ic_launcher;
+    Beta.storageDir =
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    Beta.showInterruptedStrategy = false;
+    Bugly.init(getApplicationContext(), APP_ID, true);
+  }
+
+  public static Application getInstance() {
+    return instance;
+  }
+
+  public void setGroupPartTime(int groupTime) {
+    sharedPreferences.edit().putInt(GROUP_PART_TIME_KEY, groupTime).apply();
+  }
+
+  public int getGroupPartTime() {
+    return sharedPreferences.getInt(GROUP_PART_TIME_KEY, 30);
+  }
+
+  public void setGroupRestTime(int groupTime) {
+    sharedPreferences.edit().putInt(GROUP_REST_TIME_KEY, groupTime).apply();
+  }
+
+  public int getGroupRestTime() {
+    return sharedPreferences.getInt(GROUP_REST_TIME_KEY, 5);
+  }
+
+  public void setGroupCount(int count) {
+    sharedPreferences.edit().putInt(GROUP_COUNT_KEY, count).apply();
+  }
+
+  public int getGroupCount() {
+    return sharedPreferences.getInt(GROUP_COUNT_KEY, 4);
+  }
+}
---
+++ app/src/main/java/cn/cattlefish/bodybuilding/MainActivity.java
@@ -0,0 +1,107 @@
+package cn.cattlefish.bodybuilding;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Stack;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+  TextView txtSecond;
+  ExecutorService executorService;
+  Stack<Runnable> stacks = new Stack<Runnable>();
+  int second = 0;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    txtSecond = (TextView) findViewById(R.id.txt_second);
+    final EditText edtPart = (EditText) findViewById(R.id.edit_part);
+    final EditText edtRest = (EditText) findViewById(R.id.edit_rest);
+    final EditText edtCount = (EditText) findViewById(R.id.edit_count);
+    edtPart.setText(Application.getInstance().getGroupPartTime() + "");
+    edtRest.setText(Application.getInstance().getGroupRestTime() + "");
+    edtCount.setText(Application.getInstance().getGroupCount() + "");
+    findViewById(R.id.btn_terminal).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        stopService();
+      }
+    });
+    findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        String part = edtPart.getText().toString();
+        String rest = edtRest.getText().toString();
+        String count = edtCount.getText().toString();
+        if (!TextUtils.isEmpty(part) && !TextUtils.isEmpty(rest) && !TextUtils.isEmpty(count)) {
+          final int partTime = Integer.valueOf(part);
+          final int restTime = Integer.valueOf(rest);
+          final int countNo = Integer.valueOf(count);
+          Application.getInstance().setGroupPartTime(partTime);
+          Application.getInstance().setGroupRestTime(restTime);
+          Application.getInstance().setGroupCount(countNo);
+          for (int i = 0; i < countNo; i++) {
+            Runnable runnable = new Runnable() {
+              @Override public void run() {
+                try {
+                  System.out.println("组开始");
+                  for (int j = 0; j < partTime; j++) {
+                    Thread.sleep(1000);
+                    second++;
+                    handler.sendEmptyMessage(0);
+                  }
+                  System.out.println("组结束，开始休息");
+                  for (int j = 0; j < restTime; j++) {
+                    Thread.sleep(1000);
+                    second++;
+                    handler.sendEmptyMessage(0);
+                  }
+                } catch (Exception e) {
+                  e.printStackTrace();
+                }
+              }
+            };
+            stacks.push(runnable);
+          }
+          executorService = Executors.newSingleThreadExecutor();
+          while (!stacks.isEmpty()) {
+            executorService.execute(stacks.pop());
+          }
+        } else {
+          toastShow("请填写全部参数");
+        }
+      }
+    });
+  }
+
+  private void toastShow(String msg) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+  }
+
+  private void stopService() {
+    executorService.shutdownNow();
+    executorService = null;
+    handler.removeCallbacksAndMessages(null);
+    second = 0;
+    txtSecond.setText("0s");
+    stacks.clear();
+  }
+
+  @Override protected void onDestroy() {
+    stopService();
+    super.onDestroy();
+  }
+
+  Handler handler = new Handler() {
+    @Override public void handleMessage(Message msg) {
+      super.handleMessage(msg);
+      txtSecond.setText(second + "s");
+    }
+  };
+}
